9004b8a420e5b827ae43ce9ca5250c738bab2754
msalvadores <msalvadores@gmail.com>
2013-01-25 11:57:49 -0800

    Correct endpoint stage --> production

--- java/src/org/ncbo/stanford/sparql/examples/JenaARQFederationExample.java
+++ java/src/org/ncbo/stanford/sparql/examples/JenaARQFederationExample.java
@@ -30,25 +30,25 @@ public class JenaARQFederationExample {
 		 * For documentation please read "Federated SPARQL queries"
 		 *  at http://www.bioontology.org/wiki/index.php/SPARQL_BioPortal 
 		 */
+		JenaARQFederationExample test = new JenaARQFederationExample();
 
 		String query = "PREFIX map: <http://protege.stanford.edu/ontologies/mappings/mappings.rdfs#> \n" +
 				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" +
 				"PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n" + 
 				"SELECT DISTINCT ?mappedParent WHERE {\n" + 
-				"    SERVICE <http://stagesparql.bioontology.org/mappings/sparql/?apikey=YOUR API KEY HERE> {\n" + 
+				"    SERVICE <http://sparql.bioontology.org/mappings/sparql/?apikey=YOUR API KEY HERE> {\n" + 
 				"        ?mapping map:target <http://purl.bioontology.org/ontology/CSP/0468-5952> .\n" + 
 				"        ?mapping map:source ?source .\n" + 
 				"    }\n" + 
-				"    SERVICE <http://stagesparql.bioontology.org/ontologies/sparql/?apikey=YOUR API KEY HERE> {\n" + 
+				"    SERVICE <http://sparql.bioontology.org/ontologies/sparql/?apikey=YOUR API KEY HERE> {\n" + 
 				"        ?source rdfs:subClassOf ?mappedParent .\n" +
 				"    }\n" +
 				"}";
 		
-		JenaARQFederationExample test = new JenaARQFederationExample();
 		ResultSet results = test.executeQuery(query);
-		    for ( ; results.hasNext() ; ) {
-		      QuerySolution soln = results.nextSolution() ;
-		      System.out.println(soln);
-		    }
+	    for ( ; results.hasNext() ; ) {
+	      QuerySolution soln = results.nextSolution() ;
+	      System.out.println(soln);
+	    }
 	}
 }
